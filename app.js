3edb7c12a440f95ad2396501652505ce06fc2d8a
brain-kracha <brian.kracha@gmail.com>
2019-01-20 17:47:49 -0700

    testing with CORS error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors')
 
-// var indexRouter = require('./lib/index');
+
 var productsRouter = require('./routes/index');
 
 var app = express();
@@ -13,17 +13,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
   if (req.query.delay) return setTimeout(next, parseInt(req.query.delay, 10))
   next()
 })
-// app.use('/api', indexRouter)
-app.use('/',productsRouter)
 
+app.use('/',productsRouter);
 
+// app.get('/', (req, res, next) => res.redirect('/api'))
 
 app.use(function(req, res, next) {
   const err = new Error('Not Found')
